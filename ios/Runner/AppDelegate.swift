2f32e405fd81510f49b4c8f10c7c3bd8a1423207
haoluuquoc <quochaoluuu@gmail.com>
2026-03-05 12:28:50 +0700

    Fix AppDelegate: remove invalid FlutterImplicitEngineDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,21 +3,19 @@ import UIKit
 import UserNotifications
 
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    
     // Đăng ký notification delegate để nhận notification khi app ở foreground
     if #available(iOS 10.0, *) {
       UNUserNotificationCenter.current().delegate = self
     }
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
   
   // Cho phép notification hiện khi app đang ở foreground
